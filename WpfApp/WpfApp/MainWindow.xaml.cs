ac04ffd803356bd508cc58481656fdd88391adaa
Ahmad <ahmdaf420@gmail.com>
2023-03-01 20:35:58 +0200

    .

---
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace WpfApp
+{
+
+    public partial class MainWindow : Window
+    {
+        //regex
+        private string UserNamePattern = @"^[a-zA-Z]{1}[0-9a-zA-Z]{2,9}$";
+        private string PasswordPattern = @"^(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&'()*+,\-./:;<=>?@^_`{|}~])[A-Za-z0-9_!#$%&'()*+,\-./:;<=>?@^_`{|}~]{7,12}$";
+       /* SqlConnection DbConiction = new SqlConnection(@"Data Source=LAPTOP-3NQL7H3O\\SQLEXPRESS;Initial Catalog=WPF_DB;User ID=ahmad;Password=123123asd;");*/
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           /* CheckSqlConnection();*/
+           
+        }
+     
+        //main login function
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ValidateInput())
+                {
+                    if (ValidateUser())
+                    {
+                        GetUsers();
+                        GoToAdminPage();
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        // check if user exist and user is valid 
+        private bool ValidateUser()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Connection failed: " + ex.Message);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        //get users from DB
+        private void GetUsers()
+        {
+
+
+            // Create a SqlConnection object with your connection string
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-3NQL7H3O\\SQLEXPRESS;Initial Catalog=WPF_DB;User ID=ahmad;Password=123123asd;");
+
+            // Open the connection
+            conn.Open();
+
+            // Create a SqlCommand object with the query and connection
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users ", conn);
+
+
+
+            // Execute the query and get the results
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            // Check if the query returned any rows
+            if (reader.HasRows)
+            {
+               
+                MessageBox.Show($"yay");
+               
+
+            }
+
+            // Close the reader and connection
+            reader.Close();
+            conn.Close();
+        }
+        // go to Admin page 
+        private void GoToAdminPage()
+        {
+            AdminPanel adminPage = new AdminPanel();
+            adminPage.Show();
+            this.Close();
+
+        }
+      
+        // go to register page
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Register RegisterPage = new Register();
+            RegisterPage.Show();
+            this.Close();
+        }
+
+        // input validation 
+        public bool ValidateInput()
+        {
+
+            if (username.Text == String.Empty)
+            {
+                MessageBox.Show("username is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (password.Password == String.Empty)
+            {
+                MessageBox.Show("password is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Regex.IsMatch(username.Text, UserNamePattern))
+            {
+                MessageBox.Show("Invalid username ...");
+                return false;
+            }
+
+            if (!Regex.IsMatch(password.Password, PasswordPattern))
+            {
+                MessageBox.Show("Invalid password ...");
+                return false;
+            }
+            return true;
+        }
+
+        // DB conniction Test
+        private void CheckSqlConnection()
+        {
+            // Set the connection string
+            string connectionString = "Data Source=LAPTOP-3NQL7H3O\\SQLEXPRESS;Initial Catalog=WPF_DB;User ID=ahmad;Password=123123asd;";
+
+            // Create a new SqlConnection object
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Check if the connection state is open
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        MessageBox.Show("Connection successful.");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Connection failed.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Connection failed: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ WpfApp/WpfApp/Register.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        //regex
+        private const string EmailregexPattern = @"^[a-zA-Z0-9\.\-_]+@[a-zA-Z]{2,15}(?:\.[a-zA-Z]+){1,2}$";
+        private const string UserNamePattern = @"^[a-zA-Z]{1}[0-9a-zA-Z]{2,9}$";
+        private const string PasswordPattern = @"^(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&'()*+,\-./:;<=>?@^_`{|}~])[A-Za-z0-9_!#$%&'()*+,\-./:;<=>?@^_`{|}~]{7,12}$";
+        
+        SqlConnection DbConiction = new SqlConnection(@"Data Source=LAPTOP-3NQL7H3O\\SQLEXPRESS;Initial Catalog=WPF_DB;User ID=ahmad;Password=123123asd;");
+
+
+        public Register()
+        {
+            InitializeComponent();
+            CheckSqlConnection();
+        }
+
+
+        private void RegisterBtn(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                if (ValidateInput())
+                {
+                    //establish conniction and try to create a user to insert him and if registired go to admin panel 
+
+
+
+                    GoToAdminPanel();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                //else return the error 
+                MessageBox.Show("Connection failed: " + ex.Message);
+                throw;
+
+            }
+            finally
+            {
+
+            }
+
+               
+            }
+
+        //check if user is already registeired 
+        private bool checkIfUserExist()
+        {
+            try {
+
+                if (true)
+                {
+                    MessageBox.Show("User Already registerd ");
+                }
+
+                return true;
+            }
+            catch 
+            { 
+            return false;
+            
+            }
+
+        }
+
+        // form validation + regex
+        private bool ValidateInput()
+        {
+
+            if (txtFullName.Text == String.Empty)
+            {
+                MessageBox.Show("Full Name is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtEmail.Text == String.Empty)
+            {
+                MessageBox.Show("Email is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtEmail.Text == String.Empty)
+            {
+                MessageBox.Show("Email is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Regex.IsMatch(txtEmail.Text, EmailregexPattern))
+            {
+                MessageBox.Show("Invalid Email...");
+                return false;
+            }
+            if (txtUsername.Text == String.Empty)
+            {
+                MessageBox.Show(" Username is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Regex.IsMatch(txtUsername.Text, UserNamePattern))
+            {
+                MessageBox.Show("Invalid Username...");
+                return false;
+            }
+            if (txtPassword.Password == String.Empty)
+            {
+                MessageBox.Show("Password is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtPassword.Password == String.Empty)
+            {
+                MessageBox.Show("Password is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (txtConfirmPassword.Password == String.Empty)
+            {
+                MessageBox.Show("Confirm Password is required", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (txtConfirmPassword.Password != txtPassword.Password)
+            {
+                MessageBox.Show("Passwords  is doesnt match", "Faild", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Regex.IsMatch(txtPassword.Password, PasswordPattern) || !Regex.IsMatch(txtConfirmPassword.Password, PasswordPattern))
+            {
+                MessageBox.Show("Invalid password must contain one capital and small letter and spical one charcter at least   ...");
+                return false;
+            }
+
+
+            return true;
+        }
+
+        //go to admin panel
+        private void GoToAdminPanel()
+        {
+            this.Close();
+            AdminPanel adminPage = new AdminPanel();
+            adminPage.Show();
+        }
+
+        //go to sign in page 
+        private void LoginPage_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow LoginPage = new MainWindow();
+            LoginPage.Show();
+            this.Close();
+        }
+
+        //check the database conniction 
+        private void CheckSqlConnection()
+        {
+            // Set the connection string
+            string connectionString = "Data Source=LAPTOP-3NQL7H3O\\SQLEXPRESS;Initial Catalog=WPF_DB;User ID=ahmad;Password=123123asd;";
+
+            // Create a new SqlConnection object
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Check if the connection state is open
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        MessageBox.Show("Connection successful.");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Connection failed.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Connection failed: " + ex.Message);
+                }
+                finally {
+                    connection.Close();
+                }
+            }
+        }
+
+        //clear textbox input 
+        public void Clear()
+        {
+            txtFullName.Clear();
+            txtEmail.Clear();
+            txtUsername.Clear();
+            txtPassword.Clear();
+            txtConfirmPassword.Clear();
+        }
+    }
+}
